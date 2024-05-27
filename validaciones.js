209b321bfab5cc4741183d27b212f9359655f34a
tinchowski <132467975+tinchowski@users.noreply.github.com>
2024-05-27 13:01:16 -0300

    Add files via upload
    limpieza de código

--- validaciones.js
+++ validaciones.js
@@ -1,68 +1,3 @@
-// function ValidateEmail(inputText)
-// {
-// var mailformat = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
-// if(mailformat.test(inputText))
-//     {
-// alert("¡Gracias por registrarse en Campo Mendoza!");
-// //document.form1.email_input.focus();
-// //return true;
-// }
-// else
-// {
-// alert("Correo electrónico inválido.");
-// //document.form1.email_input.focus();
-// //return false;
-// }
-// }
-
-
-//-------------------------------FUNCIONA----------------------------------//
-
-// function ValidateEmail(inputText){
-// var mailformat = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
-// if (campos_completos(inputText)==false){
-//     alert("Por favor, complete los campos faltantes.");}
-// else{
-//     if(mailformat.test(inputText) & campos_completos(inputText)==true){
-//         alert("¡Gracias por registrarse en Campo Mendoza!");}
-//     else{alert("Correo electrónico inválido.");}
-// }
-// }
-
-// function campos_completos(inputText){
-//     if(document.querySelector("#date").value !='' & document.querySelector("#fullname")!='' & document.querySelector("#cant_personas").value !='' & inputText!=''){
-//         return true;}
-//         else{return false;}
-    
-// }
-
-
-//-------------------------------FUNCIONA----------------------------------//
-
-// function valida_campos(){
-//     var mailformat = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
-//     if (campos_completos()==false){
-//         alert("Por favor, complete los campos faltantes.");}
-//         else{
-//             var mail = document.querySelector("#email").value
-//             if(mailformat.test(mail) & campos_completos(inputText)==true){
-//         alert("¡Gracias por registrarse en Campo Mendoza!");}
-//         else{
-//         alert("Correo electrónico inválido.");}
-//     }
-//     }
-    
-//     function campos_completos(){
-//         if(document.querySelector("#date").value !='' & document.querySelector("#fullname")!='' & document.querySelector("#cant_personas")!='' & document.querySelector("#fullname")!='' & document.querySelector("#email").value!=''){
-//             return true;}
-//             else{return false;}
-        
-//    }
-
-
-
-
-
 function ValidateEmail(inputText){
     var mailformat = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
     let fecha_elegida = document.querySelector("#date").valueAsNumber;
@@ -70,7 +5,7 @@ function ValidateEmail(inputText){
         alert("Por favor, complete los campos faltantes.");}
     else{
         if(mailformat.test(inputText) & campos_completos(inputText)==true){
-            if(fecha_hoy() > fecha_elegida){alert("Ingrese fecha posterior a la actual.");}
+            if(fecha_hoy() > fecha_elegida){alert("Ingrese fecha posterior a la actual."); return false;}
             else{alert("¡Gracias por registrarse en Campo Mendoza!");}
             }
             
