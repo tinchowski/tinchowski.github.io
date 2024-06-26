c84815567eb5701b2ba86920948242322cd70c9c
Luciano <>
2024-06-25 22:44:55 -0300

    Refactorizado - imagenes tratadas - todos los .js en js/actions.js - todos los .css en css/styles.css - limpieza pero de verdad

--- File.js
+++
@@ -1,5 +0,0 @@
-<script>
-    document.querySelector('.file-label').addEventListener('click', function() {
-        document.querySelector('.file-input').click();
-    });
-</script>
--- cant_per.js
+++
@@ -1,8 +0,0 @@
-// control de cantidad de invitados
-cant_personas.oninput = function () {
-    if (this.value> 10) {
-        this.value = this.value.slice(0,2);
-        alert('Máximo 10 personas.');
-        this.value = '';
-    }
-  }
